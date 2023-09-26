6c3592341e95021a8e045cdef48e5a440cd02393
Roberto Rojo <rrojo.va@gmail.com>
2023-09-26 02:51:21 +0200

    images public directory

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,7 +5,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
+    // Activate Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
