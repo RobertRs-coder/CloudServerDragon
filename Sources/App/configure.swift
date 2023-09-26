eff85a3e2e0160020a77082e42f2de4f7d3ea5b9
Roberto Rojo <rrojo.va@gmail.com>
2023-09-26 03:08:36 +0200

    Environment variables

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,12 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+
+    guard let jwtKey = Environment.get("JWT_KEY") else { fatalError("JWT not found") }
+    guard let _ = Environment.process.API_KEY else { fatalError() }
+    guard let dbURL = Environment.process.DATABASE_URL else { fatalError() }
+    guard let _ = Environment.process.APP_BUNDLE_ID else { fatalError() }
+
     // Activate Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
