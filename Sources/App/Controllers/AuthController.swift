bee0df85b29beca80d410d9c1b0aeafc7122e5fd
Roberto Rojo <rrojo.va@gmail.com>
2023-09-26 22:27:19 +0200

    signUp function with user publi return

--- Sources/App/Controllers/AuthController.swift
+++ Sources/App/Controllers/AuthController.swift
@@ -9,9 +9,9 @@ struct AuthController: RouteCollection {
             builder.post("signup", use: signUp)
         }
     }
-
+  
     // MARK: - Routes
-    func signUp(req: Request) async throws -> User {
+    func signUp(req: Request) async throws -> User.Public {
 
         let userCreate = try req.content.decode(User.Create.self)
 
@@ -19,6 +19,9 @@ struct AuthController: RouteCollection {
         let user = User(name: userCreate.name, email: userCreate.email, password: userCreate.password)
         try await user.create(on: req.db)
 
-        return user 
+        // Tranform
+        let userPublic = User.Public(id: user.id?.uuidString ?? "", name: user.name, email: user.email)
+
+        return userPublic
     } 
 }
