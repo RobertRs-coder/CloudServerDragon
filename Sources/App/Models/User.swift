ff22d405f3d95c23ec50f2605d2988f2941bbafe
Roberto Rojo <rrojo.va@gmail.com>
2023-09-26 15:58:00 +0200

    full user model

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -9,7 +9,46 @@ final class User: Model {
     @ID(key: .id)
     var id: UUID?
 
-    @Timestamp(key: "created_at", on: .create, format: .default)
-    var created_at: Date?
+    @Timestamp(key: "created_at", on: .create, format: .iso8601)
+    var createdAt: Date?
 
+    @Field(key: "name")
+    var name: String
+
+    @Field(key: "email")
+    var email: String
+
+    @Field(key: "password")
+    var password: String
+
+    // Init  - mandatory
+    init() { }
+
+    init(id: UUID? = nil, name: String, email: String, password: String) {
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+    }
+
+}
+// MARK: - User DTO's
+extension User {
+
+    struct Create: Content {
+        let name: String
+        let email: String
+        let password: String
+    }
+
+    struct SignIn: Content {
+        let email: String
+        let password: String
+    }
+
+    struct Public: Content {
+        let id: String
+        let name: String
+        let email: String
+    }
 }
