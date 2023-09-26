f65c2332e673af8e6c8d357cf1504f6b4423e700
Roberto Rojo <rrojo.va@gmail.com>
2023-09-26 03:15:13 +0200

    database connection

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,12 +7,15 @@ import Vapor
 public func configure(_ app: Application) async throws {
 
     guard let jwtKey = Environment.get("JWT_KEY") else { fatalError("JWT not found") }
-    guard let _ = Environment.process.API_KEY else { fatalError() }
-    guard let dbURL = Environment.process.DATABASE_URL else { fatalError() }
-    guard let _ = Environment.process.APP_BUNDLE_ID else { fatalError() }
+    guard let _ = Environment.process.API_KEY else { fatalError("API_KEY not found") }
+    guard let dbURL = Environment.process.DATABASE_URL else { fatalError("DATABASE_URL not found") }
+    guard let _ = Environment.process.APP_BUNDLE_ID else { fatalError("APP_BUNDLE_ID not found") }
 
     // Activate Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    // DB connection
+    try app.databases.use(.postgres(url: dbURL), as: .psql)
 
     // register routes
     try routes(app)
